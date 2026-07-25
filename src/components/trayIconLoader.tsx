1c370c1db46e3753906fe932db3353636b5ba0e3
飞小RAN <55832499+xiaofeiTM233@users.noreply.github.com>
2026-07-25 16:32:06 +0800

    feat: 托盘增加显示/隐藏贴图功能按钮

--- src/components/trayIconLoader.tsx
+++ src/components/trayIconLoader.tsx
@@ -24,6 +24,7 @@ import { AntdContext } from "@/contexts/antdContext";
 import { AppContext } from "@/contexts/appContext";
 import { AppSettingsPublisher } from "@/contexts/appSettingsActionContext";
 import { usePluginServiceContext } from "@/contexts/pluginServiceContext";
+import { toggleFixedContentVisibility } from "@/functions/fixedContent";
 import {
 	executeScreenshot,
 	executeScreenshotFocusedWindow,
@@ -427,6 +428,21 @@ const TrayIconLoaderComponent = () => {
 						: formatKey(shortcutKeys[AppFunction.FixedContent].shortcutKey),
 					action: async () => {
 						createFixedContentWindow();
+					},
+				},
+				{
+					id: `${appWindow.label}-toggle-fixed-content-visibility`,
+					text: intl.formatMessage({
+						id: "home.toggleFixedContentVisibility",
+					}),
+					accelerator: disableShortcut
+						? undefined
+						: formatKey(
+								shortcutKeys[AppFunction.ToggleFixedContentVisibility]
+									.shortcutKey,
+							),
+					action: async () => {
+						await toggleFixedContentVisibility();
 					},
 				},
 				...getPlatformValue(
